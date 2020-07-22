71ea78003a6ac3066a25af9510b2d3e51f778246
Pedro Duarte <mrnerd777@gmail.com>
2020-07-22 01:07:00 -0300

    Added feature 'button creator'.

--- venv/Lib/calcGui.py
+++ venv/Lib/calcGui.py
@@ -31,82 +31,35 @@ def equal():
         expression = ""
 
 
-def butao(txt, bg, row, column, cmd = "command = lambda: press()"):
+def butao(txt, bg, row, column, cmd):
     global janela
     butao = Button(janela, text = txt, fg = "white", bg = bg, command = cmd, height = 2,
                    width = 6)
     butao.grid(row = row, column = column)
 
-# Making Buttons #
+#  \-_=_- Making Buttons -_=_-/ #
+
 # Buttons - Numbers #
 
-butao(" 1 ", "black", 1, 0)
-
-#b_um = Button(janela, text = " 1 ", fg = "white", bg = "black", command = lambda: press(1),
-#              height = 2, width = 6)
-#b_um.grid(row = 1, column = 0)
-
-b_dois = Button(janela, text = " 2 ", fg = "white", bg = "black", command = lambda: press(2),
-              height = 2, width = 6)
-b_dois.grid(row = 1, column = 1)
-
-b_tres = Button(janela, text = " 3 ", fg = "white", bg = "black", command = lambda: press(3),
-              height = 2, width = 6)
-b_tres.grid(row = 1, column = 2)
-
-b_quatro = Button(janela, text = " 4 ", fg = "white", bg = "black", command = lambda: press(4),
-              height = 2, width = 6)
-b_quatro.grid(row = 2, column = 0)
-
-b_cinco = Button(janela, text = " 5 ", fg = "white", bg = "black", command = lambda: press(5),
-              height = 2, width = 6)
-b_cinco.grid(row = 2, column = 1)
-
-b_seis = Button(janela, text = " 6 ", fg = "white", bg = "black", command = lambda: press(6),
-              height = 2, width = 6)
-b_seis.grid(row = 2, column = 2)
-
-b_sete = Button(janela, text = " 7 ", fg = "white", bg = "black", command = lambda: press(7),
-              height = 2, width = 6)
-b_sete.grid(row = 3, column = 0)
-
-b_oito = Button(janela, text = " 8 ", fg = "white", bg = "black", command = lambda: press(8),
-              height = 2, width = 6)
-b_oito.grid(row = 3, column = 1)
-
-b_nove = Button(janela, text = " 9 ", fg = "white", bg = "black", command = lambda: press(9),
-              height = 2, width = 6)
-b_nove.grid(row = 3, column = 2)
-
-b_zero = Button(janela, text = " 0 ", fg = "white", bg = "black", command = lambda: press(0),
-              height = 2, width = 6)
-b_zero.grid(row = 4, column = 1)
-
-b_dot = Button(janela, text = " . ", fg = "white", bg = "#181818", command = lambda: press("."),
-              height = 2, width = 6)
-b_dot.grid(row = 4, column = 2)
+butao(" 1 ", "black", 1, 0, lambda: press(1))
+butao(" 2 ", "black", 1, 1, lambda: press(2))
+butao(" 3 ", "black", 1, 2, lambda: press(3))
+butao(" 4 ", "black", 2, 0, lambda: press(4))
+butao(" 5 ", "black", 2, 1, lambda: press(5))
+butao(" 6 ", "black", 2, 2, lambda: press(6))
+butao(" 7 ", "black", 3, 0, lambda: press(7))
+butao(" 8 ", "black", 3, 1, lambda: press(8))
+butao(" 9 ", "black", 3, 2, lambda: press(9))
+butao(" 0 ", "black", 4, 1, lambda: press(0))
 
 # Buttons - Operations #
 
-b_equal = Button(janela, text = " = ", fg = "white", bg = "#181818", command = equal,
-                 height = 2, width = 6)
-b_equal.grid(row = 4, column = 0)
-
-b_plus = Button(janela, text = " + ", fg = "white", bg = "#181818", command = lambda: press("+"),
-                height = 2, width = 6)
-b_plus.grid(row = 1, column = 3)
-
-b_minus = Button(janela, text = " - ", fg = "white", bg = "#181818", command = lambda:
-                press("-"), height = 2, width = 6)
-b_minus.grid(row = 2, column = 3)
-
-b_multiply = Button(janela, text = " x ", fg = "white", bg = "#181818", command = lambda:
-                    press("*"), height = 2, width = 6)
-b_multiply.grid(row = 3, column = 3)
-
-b_divide = Button(janela, text = " / ", fg = "white", bg = "#181818", command = lambda:
-                    press("/"), height = 2, width = 6)
-b_divide.grid(row = 4, column = 3)
+butao(" = ", "#181818", 4, 0, equal)
+butao(" . ", "#181818", 4, 2, lambda: press("."))
+butao(" + ", "#181818", 1, 3, lambda: press("+"))
+butao(" - ", "#181818", 2, 3, lambda: press("-"))
+butao(" x ", "#181818", 3, 3, lambda: press("*"))
+butao(" / ", "#181818", 4, 3, lambda: press("/"))
 
 # Starting GUI #
 janela.mainloop()
