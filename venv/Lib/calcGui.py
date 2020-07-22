8084491e578a9f0253a3e0cfac75ac9e470029d1
Pedro Duarte <mrnerd777@gmail.com>
2020-07-22 17:57:12 -0300

    Added new feature (Button creator in loop).

--- venv/Lib/calcGui.py
+++ venv/Lib/calcGui.py
@@ -30,29 +30,28 @@ def equal():
         display.set("ERROR ...")
         expression = ""
 
-
 def butao(txt, bg, row, column, cmd):
     global janela
     butao = Button(janela, text = txt, fg = "white", bg = bg, command = cmd, height = 2,
                    width = 6)
     butao.grid(row = row, column = column)
 
-#  \-_=_- Making Buttons -_=_-/ #
+#  \=_- Making Buttons -_=/ #
 
 # Buttons - Numbers #
 
-butao(" 1 ", "black", 1, 0, lambda: press(1))
-butao(" 2 ", "black", 1, 1, lambda: press(2))
-butao(" 3 ", "black", 1, 2, lambda: press(3))
-butao(" 4 ", "black", 2, 0, lambda: press(4))
-butao(" 5 ", "black", 2, 1, lambda: press(5))
-butao(" 6 ", "black", 2, 2, lambda: press(6))
-butao(" 7 ", "black", 3, 0, lambda: press(7))
-butao(" 8 ", "black", 3, 1, lambda: press(8))
-butao(" 9 ", "black", 3, 2, lambda: press(9))
-butao(" 0 ", "black", 4, 1, lambda: press(0))
+txt = 0 # Starting var 'txt' #
+butao(str(txt), "black", 4, 1, lambda: press(txt))
 
-# Buttons - Operations #
+# Loop responsible for create the buttons #
+
+for (r) in range(1, 4):
+    for (c) in range(0, 3):
+        txt += 1
+        butao(str(txt), "black", r, c, lambda: press(txt))
+
+
+# Buttons - Operators #
 
 butao(" = ", "#181818", 4, 0, equal)
 butao(" . ", "#181818", 4, 2, lambda: press("."))
